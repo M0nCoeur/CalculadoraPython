19c53c8dada5902f9bda4d1e310ad81b11205dee
M0nCoeur <lucas.contato020@gmail.com>
2025-07-18 23:51:26 -0300

    Implementada a função calculadora com operações básicas: adição, subtração, multiplicação e divisão.

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
-def soma(a, b):
-    return a + b
+def calculadora():
+    print("Calculadora")
+    print("Digite o valor entre 1 e 4 para a operação: ")
+    print("1 - Adição")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
 
-print ("Resultado da soma: ", soma (2, 3))
+    escolha = input("Escolha o número da operação: ")
+
+    if escolha not in ['1', '2', '3', '4']:
+        print("Número inválido!")
+
+    try:
+        num1 = int(input("Digite o primeiro número "))
+        num2 = int(input("Digite o segundo número "))
+    except ValueError:
+        print("Digita certo ai, maluco!")
+        return
+    
+    if escolha == '1':
+        resultado = num1 + num2
+        operador = '+'
+    elif escolha == '2':
+        resultado = num1 - num2
+        operador = '-'
+    elif escolha == '3':
+        resultado = num1 * num2
+        operador = '*'
+    elif escolha == '4':
+        if num2 == 0:
+            print("Erro: divisão por zero não é permitida!")
+            return
+        resultado = num1 / num2
+        operador = '/'
+
+    print(f"{num1} {operador} {num2} = {resultado}")
+
+if __name__ == "__main__":
+    calculadora()
